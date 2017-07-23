c1f93c656aa7cf42b00acda48ad456edb139547c
Pandaliha <queenphoria@gmail.com>
2017-07-23 20:00:29 +0200

    comment

--- 3.3.2 Zeichenprogramm/src/aufgabe_3_3_2_paint/DrawPane.java
+++ 3.3.2 Zeichenprogramm/src/aufgabe_3_3_2_paint/DrawPane.java
@@ -3,7 +3,7 @@ package aufgabe_3_3_2_paint;
 import javafx.scene.layout.Pane;
 
 /**
- * @author Scharab Hessan
+ * @author Saliha Hessan
  */
 public class DrawPane extends Pane {
     public DrawPane(int x, int y) {
