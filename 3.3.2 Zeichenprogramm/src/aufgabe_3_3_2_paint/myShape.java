a27084563adfe4d4754fc1d8abb2dc76e8e41120
Pandaliha <queenphoria@gmail.com>
2017-07-23 20:20:13 +0200

    comment

--- 3.3.2 Zeichenprogramm/src/aufgabe_3_3_2_paint/myShape.java
+++ 3.3.2 Zeichenprogramm/src/aufgabe_3_3_2_paint/myShape.java
@@ -3,33 +3,16 @@ package aufgabe_3_3_2_paint;
 import javafx.scene.paint.Color;
 
 /**
- *  @author Scharab Hessan
+ *  @author Saliha Hessan
  */
 public interface myShape extends myNode {
-    /**
-     * Das aufziehen der Form
-     * @param xStart
-     * @param yStart
-     * @param xPos
-     * @param yPos
-     */
+
     void draw(double xStart, double yStart, double xPos, double yPos);
 
-    /**
-     * Linienfarbe festlegen
-     * @param color
-     */
+
     void setBorderColor(Color color);
 
-    /**
-     * Linienbreite festlegen
-     * @param width
-     */
     void setBorderWidth(double width);
 
-    /**
-     * Füllfarbe festlegen
-     * @param color
-     */
     void setFillColor(Color color);
 }
